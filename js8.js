130bcadf2dbb6c8f65c8d9dad70fee2113dd6af6
RamaSanjeev <110553311+RamaSanjeev@users.noreply.github.com>
2022-08-10 19:22:57 +0530

    second commit

---
+++ js8.js
@@ -0,0 +1,7 @@
+//FIRST 10 NATURAL NUMBERS USING FUNCTION(9TH August 2022):-
+function natural(n){
+    for (let i=1;i<=n;i++){
+        console.log(i);
+    }
+}
+natural(10);
---
+++ js9.js
@@ -0,0 +1,8 @@
+//MULTIPLICATION TABLE USING FUNCTION(9th August 2022)
+function multiplication(n){
+    for (let i=1;i<=10;i++){
+        const result=n*i;
+        console.log(`${n} * ${i} = ${result}`)
+    }
+}
+multiplication(5)
